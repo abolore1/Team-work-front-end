ecc444c23dff8c905000fa45cdbbae1cb33ee44a
Abolore1 <qudus_kudos@yahoo.com>
2019-11-23 03:09:41 +0100

    feature(login-page):create login page create login component [starts]

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -3,7 +3,16 @@
  const Login = () => {
      return(
          <div>
-             <h2>Login Page</h2>
+             <h2>Sign in</h2>
+             <p >Sign into your account</p>
+             <form>
+                 <div>
+                     <input type='email' placeholder='Email id' name='email'/> <br/> <br/>
+                     <input type="password" placeholder="password" name="password"/><br/> <br/>
+                     <button value="submit">Submit</button>
+                 </div>
+             </form>
+             
          </div>
      );
  }
